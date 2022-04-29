e4af3432ef1d05728fd93a7748b49e4656ab8dd4
Amber Zimmerman <ajz1221@gmail.com>
2022-04-29 13:37:05 -0600

    begining user routes edited from mini project

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  deleteUser,
+  addThought,
+  removeThought,
+} = require("../../controllers/userController");
+
+// /api/Users
+router.route("/").get(getUsers).post(createUser);
+
+// /api/Users/:userId
+router.route("/:userId").get(getSingleUser).delete(deleteUser);
+
+// /api/Users/:userId/thoughts
+router.route("/:userId/thoughts").post(addThought);
+
+// /api/Users/:userId/thoughts/:thoughtId
+router.route("/:userId/thoughts/:thoughtId").delete(removeThought);
+
+module.exports = router;
