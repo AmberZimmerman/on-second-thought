8b113580121d9b3cc96fa9cf13c06ebb751389f1
Amber Zimmerman <ajz1221@gmail.com>
2022-04-29 12:19:18 -0600

    installing json package and making models better

--- models/Thought.js
+++ models/Thought.js
@@ -1,46 +1,65 @@
-const { Schema, model } = require('mongoose');
-const User = require('./User');
+const { Schema, model } = require("mongoose");
+const User = require("./User");
+
+const reactionSchema = new mongoose.Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+    },
+    reactionBody: {
+      type: String,
+      minlength: 1,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
 
 // Schema to create Thought model
 const thoughtSchema = new Schema(
-    {
-        thoughtText: {
-            type: { String, minlength: 1, maxlength: 280 },
-            required: true,
-        },
-        /*Date
-        Set default value to the current timestamp
-        Use a getter method to format the timestamp on query*/ 
-        createdAt: {
-            timestamps: true,
-        },
-        username: {
-            type: String,
-            required: true,
-            postedBy: User,
-        }
+  {
+    thoughtText: {
+      type: String,
+      minlength: 1,
+      maxlength: 280,
+      required: true,
     },
-    {
-        reaction:
-        [reactionSchema],
-        reactionId: [{ type: Schema.Types.ObjectId, ref: 'reactions' }],
-        reactionBody: { String, minlength: 1, maxlength: 280 },
-        required: true,
-        username:
+    createdAt: {
+      type: Date,
+      default: Date.now,
     },
-    {
-        toJSON: {
-        getters: true,
-      },
-    }
-)
+    username: {
+      type: String,
+      required: true,
+    },
+    reaction: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
 
-/*Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.*/ 
+/*Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.*/
 
-thoughtSchema.virtual('reactionCount').get(function () {
-    return this.reaction.length;
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reaction.length;
 });
 
-const Thought = model('thought', thoughtSchema);
+const Thought = model("thought", thoughtSchema);
 
 module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -1,78 +1,58 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 // { $trim: { input: <string>,  chars: <string> } }
 // Do I add trim as trim: true here or do I add it elsewhere
 
 // Schema to create User model
 const userSchema = new Schema(
-    {
-        username: {
-            type: String,
-            unique: true,
-            require: true,
-            trim: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            // add in match a valid email
-        },
-        thoughts:
-        [thoughtsSchema],
-        
-        friends:
-        [userSchema],
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
     },
-    {
-        toJSON: {
-            virtuals: true,
-        },
-        id: false,
-    }
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "thought",
+      },
+    ],
+
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
 );
 
 // Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
 
-userSchema.virtual('friendCount').get(function () {
-    return this.friends.length;
-})
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
 
-const User = model ('user', userSchema);
+const User = model("user", userSchema);
 
 module.exports = User;
 
-
-
-
-/*import { isEmail } from 'validator';
-// ... 
-
-const EmailSchema = new Schema({
-    email: { 
-        //... other setup
-        validate: [ isEmail, 'invalid email' ]
-    }
-});
-
-
-
-var validateEmail = function(email) {
-    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    return re.test(email)
-};
-
-var EmailSchema = new Schema({
-    email: {
-        type: String,
-        trim: true,
-        lowercase: true,
-        unique: true,
-        required: 'Email address is required',
-        validate: [validateEmail, 'Please fill a valid email address'],
-        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
-    }
-});
-
-https://stackoverflow.com/questions/18022365/mongoose-validate-email-syntax
-*/ 
+/* stack article on email validation
+https://stackoverflow.com/questions/18022365/mongoose-validate-email-syntax/
+*/
